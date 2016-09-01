c6d63cb45d6ae64e136a66700aae05c07206c41c
judasnow <judasnow@gmail.com>
2016-09-01 17:46:37 +0800

    club 增删查改

--- client.go
+++ client.go
@@ -3,38 +3,91 @@ package main
 import (
 	clubProto "baymax/club_srv/protocol/club"
 	"baymax/rpc/client"
-	"fmt"
-	"log"
-	"time"
 	"flag"
+	"log"
+	"math/rand"
 )
 
-func testClubGet(c *client.Client) error {
+// 测试获取单条俱乐部信息
+func testClubGetOne(c *client.Client) error {
 	var reply clubProto.GetResponse
 
-	err := c.Call(clubProto.ServiceGetClub, &clubProto.GetRequest{ClubId: 1024}, &reply)
+	err := c.Call(clubProto.ServiceGetClub, &clubProto.GetRequest{ClubID: 45}, &reply)
 	if err != nil {
 		return err
 	} else {
-		fmt.Println(reply)
+		log.Print(reply)
 		return nil
 	}
 }
 
-func testClubCreate(_ *client.Client) error {
+// 测试创建新的俱乐部
+func testClubCreate(c *client.Client) error {
+	var (
+		req clubProto.CreateRequest
+		res clubProto.CreateResponse
+		err error
+	)
+
+	req.Name = "2048"
+
+	err = c.Call(clubProto.ServiceCreateClub, &req, &res)
+	if err != nil {
+		return err
+	} else {
+		log.Print(res)
+		return nil
+	}
+}
+
+func testClubUpdate(c *client.Client) error {
+
+	var (
+		req clubProto.UpdateRequest
+		res clubProto.UpdateResponse
+		err error
+	)
+
+	req.Name = (string)(rand.Intn(1024))
+	req.ID = 316
+
+	err = c.Call(clubProto.ServiceUpdateClub, &req, &res)
+	if err != nil {
+		return err
+	} else {
+		log.Print(res)
+	}
+
 	return nil
 }
 
-func testClubGetAllCreate(_ *client.Client) error {
-	return nil
+func testClubDelete(c *client.Client) error {
+
+	var (
+		req clubProto.DeleteRequest
+		res clubProto.DeleteResponse
+		err error
+	)
+
+	req.ClubID = 317
+	err = c.Call(clubProto.ServiceDeleteClub, &req, &res)
+	if err != nil {
+		return err
+	} else {
+		log.Print(err)
+		return nil
+	}
 }
 
-func testClubDelete(_ *client.Client) error {
-	return nil
-}
+// func testClubGetMany(c *client.Client) error {
+// 	return nil
+// }
 
 func main() {
-	var port string
+	var (
+		port string
+		err error
+	)
 
 	flag.StringVar(&port, "port", "8080", "server port")
 	flag.Parse()
@@ -42,7 +95,24 @@ func main() {
 	c := client.NewClient("tcp", ":" + port)
 	defer c.Close()
 
-	err := testClubGet(c)
+	// 测试 club 部分的 rpc
+
+	err = testClubGetOne(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = testClubCreate(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = testClubDelete(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = testClubUpdate(c)
 	if err != nil {
 		log.Fatal(err)
 	}
--- club_srv/handler/handler.go
+++ club_srv/handler/handler.go
@@ -3,23 +3,88 @@ package handler
 import (
 	"baymax/club_srv/model"
 	proto "baymax/club_srv/protocol/club"
+	"fmt"
 )
 
 type ClubHandler struct {}
 
-func (hdl *ClubHandler) Get(req *proto.GetRequest, resp *proto.GetResponse) error {
+func (*ClubHandler) Get(req *proto.GetRequest, resp *proto.GetResponse) error {
 
 	club := model.Club{}
-	err := model.DB.Where("id = ?", req.ClubId).First(&club).Error
+	err := model.DB.Where("id = ?", req.ClubID).First(&club).Error
 
 	if err != nil {
 		return err
 	} else {
-		resp.Data = []proto.Club{}
+		c, err := proto.InitFromModel(&club)
+		if err != nil {
+			return err
+		} else {
+			resp.Data = []proto.Club{c}
+			return nil
+		}
+	}
+}
+
+// 创建新的 club 返回 id
+func (*ClubHandler) Create(req *proto.CreateRequest, resp *proto.CreateResponse) error {
+
+	club := model.Club{}
+
+	// TODO 两种类型的 struct 之间的转换
+	club.Name = req.Name
+
+	var err error
+	err = model.DB.Create(&club).Error
+
+	if err != nil {
+		return err
+	} else {
+		resp.ClubID = club.ID
 		return nil
 	}
 }
 
-func (hdl *ClubHandler) Create(req *proto.CreateRequest, resp *proto.CreateResponse) error {
+// 删除指定的 club
+func (*ClubHandler) Delete(req *proto.DeleteRequest, resp *proto.DeleteResponse) error {
+
+	club := model.Club{ID: req.ClubID}
+
+	var err error
+	// 原数据库没有 delete_at 字段, 指定记录会被直接删除
+	err = model.DB.Delete(&club).Error
+	if err != nil {
+		return err
+	} else {
+		resp.ClubID = club.ID
+	}
+
 	return nil
+}
+
+// 更新指定的 club
+func (*ClubHandler) Update(req *proto.UpdateRequest, res *proto.UpdateResponse) error {
+	club :=	model.Club{}
+
+	var err error
+	err = model.DB.Where("id = ?", req.Club.ID).First(&club).Error
+
+	if err != nil {
+		return err
+	} else {
+		club.Name = req.Name
+
+		err = model.DB.Model(&club).Update().Error
+		if err != nil {
+			return err
+		} else {
+			(*res).Club, err = proto.InitFromModel(&club)
+			if err != nil {
+				return err
+			} else {
+				return nil
+			}
+		}
+	}
+
 }
