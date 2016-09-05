2bac9a5b1ab680d2bd7a2e6a9f5378f5e065a238
judasnow <judasnow@gmail.com>
2016-09-05 17:07:27 +0800

    修改测试文件

--- club_srv/handler/handler_test.go
+++ club_srv/handler/handler_test.go
@@ -7,23 +7,36 @@ import (
 	clubProto "baymax/club_srv/protocol/club"
 	"baymax/rpc"
 	"time"
-	"log"
 )
 
-const SERVER_ADDR = "127.0.0.1:8091"
+func getConn() *rpc.Client {
+	return rpc.NewClient("tcp", "127.0.0.1:8091", 10 * time.Minute)
+}
 
+// TODO 启动一个 server
 
-// 测试获取单条记录
+// 获取单条记录
 func TestGetOneClub(t *testing.T) {
-
-	c := rpc.NewClient("tcp", SERVER_ADDR, 10 * time.Minute)
-
+	c := getConn()
 	var reply clubProto.GetResponse
 
 	err := c.Call(clubProto.ServiceGetClub, &clubProto.GetRequest{ClubID: 45}, &reply)
 	if err != nil {
 		t.Error(err)
 	} else {
-		log.Println(reply)
+		t.Log(reply)
+	}
+}
+
+// 根据 ID 获取多条记录
+func TestGetManyClubs(t *testing.T) {
+	c := getConn()
+	var reply clubProto.GetManyResponse
+
+	err := c.Call(clubProto.ServiceGetManyClub, &clubProto.GetManyRequest{}, &reply)
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(reply)
 	}
 }
