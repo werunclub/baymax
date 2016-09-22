dab9a8478ff711c279ded4347c551e0ab0ea770d
ruanyingjie <ruanyj@codoon.com>
2016-09-22 10:19:53 +0800

    更改文件名并增加注释

--- util/time.go
+++ util/time.go
@@ -2,10 +2,12 @@ package util
 
 import "time"
 
-func TimeAccurateToDay(t *time.Time) time.Time {
+// 精确time.Time到天
+func TimeAccurateToDay(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
 
-func TimeAccurateToSecond(t *time.Time) time.Time {
+// 精确time.Time到秒
+func TimeAccurateToSecond(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), 0, t.Location())
 }
