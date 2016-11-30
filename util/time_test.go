47a0bf5ef69800305e5bcdb5ec536277a1a1f6ab
ruanyingjie <ruanyj@codoon.com>
2016-11-30 10:47:36 +0800

    增加主版本的周计算方法

--- util/time_test.go
+++ util/time_test.go
@@ -8,14 +8,16 @@ import (
 
 func TestMaxTime(t *testing.T) {
 	a := time.Now()
-	b := a.AddDate(0, 0, -1)
+	b := a.AddDate(-1, 0, -1)
 	c := a.AddDate(0, 0, -100)
 	d := a.AddDate(0, 0, 10)
-	e := a.AddDate(0, 0, 99)
+	e := a.AddDate(1, 0, 99)
+	f := a.AddDate(1, 0, 99)
 
 	maxTime := MaxTime(a, b, c, d, e)
 	assert.Equal(t, maxTime, e)
+	assert.Equal(t, maxTime, f)
 
 	minTime := MinTime(a, b, c, d, e)
-	assert.Equal(t, minTime, c)
+	assert.Equal(t, minTime, b)
 }
