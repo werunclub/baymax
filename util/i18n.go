c93eec33df94b1be078761e2ecc8bf91c071af17
梁小波 <liangxb@codoon.com>
2018-03-13 09:59:11 +0800

    增加自动初始化翻译方法

--- util/i18n.go
+++ util/i18n.go
@@ -2,11 +2,26 @@ package util
 
 import (
 	"context"
+	"os"
 
 	"github.com/nicksnyder/go-i18n/i18n"
 
 	"baymax/rpc/helpers"
 )
+
+var (
+	// T 翻译方法
+	T i18n.TranslateFunc
+)
+
+func init() {
+	defaultLanguage := os.Getenv("DEFAULTLANGUAGE")
+	if defaultLanguage == "" {
+		defaultLanguage = "zh-Hans"
+	}
+
+	T, _ = i18n.Tfunc(defaultLanguage)
+}
 
 // TfuncForRPC 返回翻译方法
 func TfuncForRPC(ctx context.Context, defaultLanguage string) (i18n.TranslateFunc, error) {
